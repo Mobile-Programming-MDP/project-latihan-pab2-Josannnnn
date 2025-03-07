ace95fea047f3322437cc5068a2b884dde43252e
Jonathan <Jonathanss999@gmail.com>
2025-03-07 18:52:21 +0700

    update api service
    123

--- pilem/lib/services/api_service.dart
+++ pilem/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey = 'f688fa82e5b681abfaec90a4007939dd';
+  static const String apiKey = '3b2e6de1ccbe84b51270b0e067456ec6';
   Future<List<Map<String, dynamic>>> getAllMovies() async {
     final response = await http.get(
       Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"),
