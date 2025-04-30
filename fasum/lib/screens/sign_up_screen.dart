637c51b08d053f4f6e341dc62cc5fc4464afb5fd
Kevin <Jonathanss999@gmail.com>
2025-04-30 18:30:33 +0700

    update sign screen
    screen

--- fasum/lib/screens/sign_up_screen.dart
+++ fasum/lib/screens/sign_up_screen.dart
@@ -1,93 +1,88 @@
-import 'package:fasum/screens/sign_in_screen.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum/screens/home_screen.dart';
+import 'package:fasum/screens/sign_up_screen.dart';
 
-class SignUpScreen extends StatefulWidget {
-  const SignUpScreen({super.key});
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key});
 
   @override
-  State<SignUpScreen> createState() => _SignUpScreenState();
+  SignInScreenState createState() => SignInScreenState();
 }
 
-class _SignUpScreenState extends State<SignUpScreen> {
+class SignInScreenState extends State<SignInScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  final _confirmPasswordController = TextEditingController();
+
+  String _errorMessage = '';
 
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: const Text('Registrasi Akun'),
+        title: const Text('Sign In'),
       ),
       body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                TextField(
-                    controller: _emailController,
-                    decoration:
-                        const InputDecoration(labelText: 'Email'),
-                  ),
-                TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    decoration:
-                        const InputDecoration(labelText: 'Password'),
-                  ),
-                TextField(
-                    controller: _confirmPasswordController,
-                    obscureText: true,
-                    decoration:
-                        const InputDecoration(labelText: 'Konfirmasi Password'),
-                  ),
-                Container(
-                  margin: const EdgeInsets.only(top: 16.0),
-                  child: ElevatedButton(
-                    onPressed: (){
-                      _registerAccount();
-                    }, 
-                    child: const Text('Daftar'),
-                  ),
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const SizedBox(height: 32.0),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
                 ),
-              ],
-            ),
+              ),
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: ()async {
+                  try{
+                    await FirebaseAuth.instance.signInWithEmailAndPassword(
+                      email: _emailController.text, 
+                      password: _passwordController.text,
+                    );
+                    Navigator.of(context).pushReplacement(
+                      MaterialPageRoute(
+                        builder: (context) => const HomeScreen()),
+                    );
+                  }catch (error){
+                    setState(() {
+                      _errorMessage = error.toString();
+                    });
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: Text(_errorMessage),
+                      ),
+                    );
+                  }
+                }, 
+                child: const Text('Sign In'),
+              ),
+              const SizedBox(height: 32.0),
+              TextButton(
+                onPressed: (){
+                  Navigator.push(
+                    context, 
+                    MaterialPageRoute(
+                      builder: (context) => const SignUpScreen()),
+                  );
+                }, 
+                child: const Text('Don\'t have an account? Sign Up'),
+              ),
+            ],
           ),
-        );
-      }
-
-  void _registerAccount() async{
-    if (_passwordController.text != _confirmPasswordController.text){
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Password dan konfirmasi password tidak sama'),
-        ),
-      );
-      return;
-    }
-
-    try{
-      await FirebaseAuth.instance.createUserWithEmailAndPassword(
-        email: _emailController.text, 
-        password: _passwordController.text,
-      );
-      if (mounted){
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const SignInScreen(),
-          ),
-        );
-      }
-    } on FirebaseAuthException catch (e){
-      if (mounted){
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Gagal mendaftar: ${e.message}'),
-          ),
-        );
-      }
-    }
+        ),),
+    );
   }
 }
