d1952204e3cbb6d5fce60b287b0a39650212816b
Kevin <Jonathanss999@gmail.com>
2025-04-30 17:45:44 +0700

    update screen dart
    update

--- pilem/lib/screens/detail_screen.dart
+++ pilem/lib/screens/detail_screen.dart
@@ -1,8 +1,5 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:pilem/models/movie.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class DetailScreen extends StatefulWidget {
   final Movie movie;
@@ -14,42 +11,6 @@ class DetailScreen extends StatefulWidget {
 }
 
 class DetailScreenState extends State<DetailScreen> {
-  bool _isFavorite = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkIsFavorite();
-  }
-  Future<void> _checkIsFavorite() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _isFavorite = prefs.containsKey('movie_${widget.movie.id}');
-    });
-  }
-  
-  Future<void> _toggleFavorite() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    setState(() {
-      _isFavorite = !_isFavorite;
-    });
-    if (_isFavorite) {
-      final String movieJson = jsonEncode(widget.movie.toJson());
-      prefs.setString('movie_${widget.movie.id}', movieJson);
-
-      List<String> favoriteMovieIds =
-        prefs.getStringList('favoriteMovies') ?? [];
-      favoriteMovieIds.add(widget.movie.id.toString());
-      prefs.setStringList('favoriteMovies', favoriteMovieIds);
-    } else {
-      prefs.remove('movie_${widget.movie.id}');
-      
-      List<String> favoriteMovieIds =
-        prefs.getStringList('favoriteMovies') ?? [];
-      favoriteMovieIds.remove(widget.movie.id.toString());
-      prefs.setStringList('favoriteMovies', favoriteMovieIds);
-    }
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -62,26 +23,11 @@ class DetailScreenState extends State<DetailScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Stack(
-                children: [
-                  Image.network(
-                    'https://image.tmdb.org/t/p/w500${widget.movie.backdropPath}',
-                    height: 300,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  ),
-                  Positioned(
-                    bottom: 8,
-                    right: 8,
-                    child: IconButton(
-                      icon: Icon(
-                        _isFavorite ? Icons.favorite : Icons.favorite_border,
-                          color: Colors.red,
-                      ),
-                      onPressed: _toggleFavorite,
-                    ),
-                  ),
-                ],
+              Image.network(
+                'https://image.tmdb.org/t/p/w500${widget.movie.backdropPath}',
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
               ),
               const SizedBox(height: 20),
               const Text(
@@ -93,10 +39,7 @@ class DetailScreenState extends State<DetailScreen> {
               const SizedBox(height: 20),
               Row(
                 children: [
-                  const Icon(
-                    Icons.calendar_month, 
-                    color: Colors.blue
-                  ),
+                  const Icon(Icons.calendar_month, color: Colors.blue),
                   const SizedBox(width: 10),
                   const Text(
                     'Release Date:',
--- pilem/lib/services/api_service.dart
+++ pilem/lib/services/api_service.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static const String baseUrl = "https://api.themoviedb.org/3";
-  static const String apiKey = '0040c3c99d3c83daa8d32ae144ee192e';
+  static const String apiKey = '2af55135711a91f3b077e3e21d665b9e';
 
   Future <List<Map<String, dynamic>>> getAllMovies() async {
     final response = await http.get(Uri.parse("$baseUrl/movie/now_playing?api_key=$apiKey"));
